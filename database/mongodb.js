e20c7e7e62a3fdf74b5957c92048c9fca2aefe06
gorkemmert <grkmrt1@gmail.com>
2024-09-30 14:21:19 +0300

    fix

--- database/mongodb.js
+++ database/mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 module.exports = () => {
-    const connection = "mongodb+srv://grkmrt1:pallmall_321@node-rest-shop.uv4kwhd.mongodb.net/?retryWrites=true&w=majority&appName=person"
+    const connection = "your connection"
 
     mongoose.connect(connection)
     .then(() => console.log('Connected!'));
